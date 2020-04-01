fd0a53c9312f7862d15e7fc1222ac2b7fae7ba48
Shawn Benny <sbenn9210@gmail.com>
2020-04-01 09:07:09 -0500

    bugs 1 through 3

--- bug1/index.js
+++ bug1/index.js
@@ -4,7 +4,7 @@
 //   string representing the transactions
 //*******************************************************
 function renderTransactions(transactions) {
-	var finalHTML = '<div class="buffer">TRANSACTIONS</div>;
+	var finalHTML = '<div class="buffer">TRANSACTIONS</div>'
 
 	var transactionsHTML = transactions.map(function (transaction) {
 		var transactionHTML = `
